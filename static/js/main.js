077706ee14f882e9d7409a1f091c08df5af1f5d9
techsolutionss <aulematins@gmail.com>
2023-12-07 08:17:59 -0800

    changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 // a function to display messages to the user  
-function displayResult(result,error,info,message,status){
+function displayResult(result,error,info,message,status="success"){
     if(status == "success"){
         if(result.classList.contains("d-none")){ result.classList.remove("d-none") }
                 
@@ -28,6 +28,51 @@ function displayResult(result,error,info,message,status){
     }
 }
 
+// a function to replace all whitespace in the input value 
+function trimAge(age){
+    var trimmed_age = ""
+    for(let x=0; x< age.length; x+=1){
+        if(age[x] == " "){
+            continue
+        }
+        trimmed_age+=age[x]
+    }
+    return trimmed_age
+}
+
+
+function validateInput(age){
+    var message = ""
+    var validated = true
+    age = trimAge(age) // trim age of any white spaces
+    
+    // check if the age field is non empty
+    if(!age){
+        message = "age field must not be empty"   // message to be displayed to user
+        validated = false
+    }
+    // check if age input is a valid number
+    else if(isNaN(age)){
+        var message = "age must be a valid number!"   // message to be displayed to user
+        validated = false
+    }
+    
+    // check if age input is less than zero
+    else if (age <= 0){
+        var message = "Age must not be less that or equal to zero!"
+        validated = false
+    }
+    else{
+        message = age   
+    }
+    return {
+        "validated":validated,
+        "message":message
+    }
+
+}
+
+
 //  a function to check users eligibilty to drive based on their age
 function checkEligibility(e){
     // prevent the form from submitting 
@@ -39,34 +84,19 @@ function checkEligibility(e){
     var info = document.querySelector("#info")
 
     // input validation starts here
-    
-    // check if the age field is non empty
-    if(!age){
-        var message = "age field must not be empty"   // message to be displayed to user
-        displayResult(result,error,info,message=message,status="error")
-        return false
+    var validated = validateInput(age)
+    if(!validated.validated){
+        displayResult(result,error,info,message=validated.message,status="error")
+        return
+    }else{
+        age = validated.message
     }
-    
-    // check if age input is a valid number
-    if(isNaN(age)){
-        var message = "age must be a number!"   // message to be displayed to user
-        displayResult(result,error,info,message=message,status="error")
-        return false
-    }
-    
-    // check if age input is less than zero
-    if(age <= 0){
-        var message = "Age must not be less that or equal to zero!"
-        displayResult(result,error,info,message=message,status="error")
-        return false
-    }
-    
     // input validation ends here
 
     // if age is greater than 70 then the user is not eligible to drive
     if(age >= 18){
         var message  = `age <strong>${age}</strong> is eligible to vote`   // message to be displayed to user
-        displayResult(result,error,info,message=message,status="info")
+        displayResult(result,error,info,message=message)
     }
     //  if age is less than 18 then the user is too young to drive
     else{
